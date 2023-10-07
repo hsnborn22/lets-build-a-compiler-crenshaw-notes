9622fe2e9972e97a776115475cae64f9ea675fd9
hsnborn22 <146590127+hsnborn22@users.noreply.github.com>
2023-10-07 13:08:27 +0200

    Commit #4
    implemented support for multiplication,division and order of operations via the stack.

--- compiler/cradle.pas
+++ compiler/cradle.pas
@@ -89,16 +89,50 @@ begin
     GetChar;
 end;
 {--------------------------------------------------------------}
-procedure Term;
+{---------------------------------------------------------------}
+{ Parse and Translate a Math Factor }
+procedure Factor;
 begin
     EmitLn('MOVE #' + GetNum + ',D0')
 end;
+{--------------------------------------------------------------}
+{ Recognize and Translate a Multiply }
+procedure Multiply;
+begin
+    Match('*');
+    Factor;
+    EmitLn('MULS (SP)+,D0');
+end;
+{-------------------------------------------------------------}
+{ Recognize and Translate a Divide }
+procedure Divide;
+begin
+    Match('/');
+    Factor;
+    EmitLn('MOVE (SP)+,D1');
+    EmitLn('DIVS D1,D0');
+end;
+{---------------------------------------------------------------}
+{ Parse and Translate a Math Term }
+procedure Term;
+begin
+    Factor;
+    while Look in ['*', '/'] do begin
+        EmitLn('MOVE D0,-(SP)');
+        case Look of
+        '*': Multiply;
+        '/': Divide;
+        else Expected('Mulop');
+        end;
+    end;
+end;
+{--------------------------------------------------------------}
 { Recognize and Translate an Add }
 procedure Add;
 begin
     Match('+');
     Term;
-    EmitLn('ADD D1,D0');
+    EmitLn('ADD (SP)+,D0');
 end;
 {-------------------------------------------------------------}
 { Recognize and Translate a Subtract }
@@ -106,7 +140,7 @@ procedure Subtract;
 begin
     Match('-');
     Term;
-    EmitLn('SUB D1,D0');
+    EmitLn('SUB (SP)+,D0');
     EmitLn('NEG D0');
 end;
 {---------------------------------------------------------------}
@@ -114,11 +148,13 @@ end;
 procedure Expression;
 begin
     Term;
-    EmitLn('MOVE D0,D1');
-    case Look of
+    while Look in ['+', '-'] do begin
+        EmitLn('MOVE D0,-(SP)');
+        case Look of
         '+': Add;
         '-': Subtract;
-    else Expected('Addop');
+        else Expected('Addop');
+        end;
     end;
 end;
 {--------------------------------------------------------------}
